867239bf65f81267aad9397ed549cca7f536b62e
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2024-11-06 12:02:42 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import ContactsRoutes from "./Routes/contacts.routes.js";
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// // connect to db
+// connectDB();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Routes
+app.use("/api/contacts", ContactsRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
